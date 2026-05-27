ce37f1fccc0e4e91c1784b39be421838e721e47f
no_idea <newcomer_0@163.com>
2026-05-28 03:08:18 +0800

    自定义端口

--- Back_end/run.py
+++ Back_end/run.py
@@ -1,7 +1,15 @@
+import sys
 from flask_api import app
 import os
 
 # 解决 Windows 上 OpenMP 重复加载问题
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+if "__main__" == __name__:
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    if len(sys.argv) < 2:
+        print("请传入端口号，例如: py main.py 5000")
+        sys.exit(1)
 
-app.run(host='127.0.0.1', port=5000, debug=True, threaded=False)
+    # 获取第二个参数（索引 1）
+    port = int(sys.argv[1])
+    print(f"收到端口参数: {port}")
+    app.run(host='127.0.0.1', port=port, threaded=False)
